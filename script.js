dfe7aa76b546da53b3ab2fb019922e6c69d93cce
michaelxie1 <michaelxie12345@gmail.com>
2023-10-04 19:49:20 -0400

    Made transitions smoother between pictures

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Set the opacity to 1 to make the content visible
   body.style.opacity = "1";
-});
-document.addEventListener("DOMContentLoaded", function () {
+
   var slideIndex = 0;
+  var slides = document.querySelectorAll("#slideshow-container li");
+
+  function showSlides() {
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = "0"; // Set opacity to 0 for all slides
+      slides[i].style.display = "none"; // Hide all slides
+    }
+
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+
+    slides[slideIndex].style.display = "block"; // Display the current slide
+    setTimeout(function () {
+      slides[slideIndex].style.opacity = "1"; // Fade in the current slide
+    }, 150); 
+  }
+
   showSlides();
 
   // Get the "Next" and "Previous" button elements
@@ -15,37 +35,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Add click event listeners to the "Next" and "Previous" buttons
   nextBtn.addEventListener("click", function () {
-      slideIndex++;
-      showSlides();
+    slideIndex++;
+    showSlides();
   });
 
   prevBtn.addEventListener("click", function () {
-      slideIndex--;
-      showSlides();
+    slideIndex--;
+    showSlides();
   });
 
   // Add event listener for arrow key presses
   document.addEventListener("keydown", function (event) {
-      if (event.key === "ArrowLeft") {
-          slideIndex--;
-          showSlides();
-      } else if (event.key === "ArrowRight") {
-          slideIndex++;
-          showSlides();
-      }
+    if (event.key === "ArrowLeft") {
+      slideIndex--;
+      showSlides();
+    } else if (event.key === "ArrowRight") {
+      slideIndex++;
+      showSlides();
+    }
   });
-
-  function showSlides() {
-      var slides = document.querySelectorAll("#slideshow-container li");
-      for (var i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      if (slideIndex >= slides.length) {
-          slideIndex = 0;
-      }
-      if (slideIndex < 0) {
-          slideIndex = slides.length - 1;
-      }
-      slides[slideIndex].style.display = "block";
-  }
 });
+
